7d4d083391ef40b21065e9c9189171a413956ec5
vsaiadian <veronikasaiadian@gmail.com>
2024-11-21 12:10:21 -0800

    Add files via upload
    I am sending this version with sounds so you can have it, while I am working in a separate file on my level. Lmk if you want to be the one to create connections between levels, like deiconcify windows and stuff. Look at Cross's files.

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import tkinter as tk
 from tkinter import *
+import pygame
 from tkinter.ttk import *
 from PIL import ImageTk, Image #importing Pillow library to manipulate images
-window = Tk()
-window.geometry("800x600")
+windowMain = Tk()
+windowMain.geometry("800x600")
 
 windowIcon = PhotoImage(file = "lockLogo.png")#variable that stores Image
-window.iconphoto(False, windowIcon)#method that changes the window icon
+windowMain.iconphoto(False, windowIcon)#method that changes the window icon
 
-window.title("PHANTASM")
+windowMain.title("PHANTASM")
+
+pygame.init() #pygame initialization
+
+#Sounds
+mainScreenBg = "mainMusic.wav"
+pygame.mixer.music.load(mainScreenBg) #loads long audios into pygame music player and makes it playable
+
+click_sound = pygame.mixer.Sound("button_click.wav")#using pygame, makes a sound object that can be then played, stopped, etc
+
+
 
 def clickTrack():
     print("Successful button click")#for our reference in the console
 
-def game_pause(status):
-    if status==True:
-        
-        # configure the pause screen and have resume button call resume function
-    else:
-        
-        # Resume animations or game loop
+def start_game(mode):
+    """Switch from start screen to game screen."""
+    # Stop background music
+    pygame.mixer.music.stop() #stops the music module from playing the loaded file
 
-# Function to resume the game
-def resume_game():
-    global is_paused
-    is_paused = False
-    #pause_label.config(text="Game Running")
-    resume_button.pack_forget()  # Hide the Resume button? idk how i should go abt this in depth
-
-def start_game(mode): #based on input gives different difficulty of game
+    # Play button click sound
+    click_sound.play()
+    
+    
+    windowMain.withdraw() 
+    level1_window.deiconify()  # restores/shows the hidden window
     if mode == "regular":
+        print()
         # Add logic for Regular mode level 1 edgar
     elif mode == "nightmare":
+        print()
         #hard lvl
+        #drop down for pause and quit
+        
+        # configure the pause screen and have resume button call resume function
+
+# Function to resume the game
+ # Hide the Resume button? idk how i should go abt this in depth
+
+    
         
 
 firstPageImg = Image.open("firstPage.png") #making sure the image is accessible
@@ -40,8 +56,10 @@ firstPageImg = firstPageImg.resize((800, 600), Image.LANCZOS)#Image.LANCZOS is f
 
 game_bg = ImageTk.PhotoImage(firstPageImg)#takes an image from Pillow library and converts tkinter image
 
-canvas = Canvas(window, width=800, height=600)#canvas widget adding
+canvas = Canvas(windowMain, width=800, height=600)#canvas widget adding
 canvas.pack(fill=tk.BOTH, expand=True)
+
+pygame.mixer.music.play(-1)#makes the music loaded into
 
 canvas.create_image(0, 0, anchor="nw", image=game_bg)
 canvas.create_text(400, 110, text = "PHANTASM", font=("Fixedsys", 73))#adding text on top of canvas
@@ -52,7 +70,7 @@ canvas.create_text(400, 170, text = "Choose game mode:", font=("Fixedsys", 23))#
 ButtonImage = Image.open("rectangularButton.png")#loading an image in pillow library to make an image object that can be worked with
 ButtonImage = ButtonImage.resize((140, 90), Image.LANCZOS)
 ButtonImage = ImageTk.PhotoImage(ButtonImage)#converts image to format working for tkinter(from PIL)/prep to use in tk
-buttonReg = tk.Button(window, image = ButtonImage, background="grey", command=lambda: start_game("regular"))
+buttonReg = tk.Button(windowMain, image = ButtonImage, background="grey", command=lambda: start_game("regular"))
 buttonReg.image = ButtonImage #this button object will be referencing sBI image object
 canvas.create_window(265, 450, window = buttonReg)
 
@@ -60,10 +78,17 @@ canvas.create_window(265, 450, window = buttonReg)
 nightButtonImage = Image.open("nightButton.png")#loading an image in pillow library to make an image object that can be worked with
 nightButtonImage = nightButtonImage.resize((140, 90), Image.LANCZOS)
 nightButtonImage = ImageTk.PhotoImage(nightButtonImage)
-buttonNight = tk.Button(window, image = nightButtonImage, background="grey")
+buttonNight = tk.Button(windowMain, image = nightButtonImage, background="grey", command=lambda: start_game("nightmare"))
 buttonNight.image = nightButtonImage #this button object will be referencing sBI image object
 canvas.create_window(565, 450, window = buttonNight)
-window.mainloop()
+windowMain.mainloop()
+
+level1_window = tk.Toplevel(windowMain)  # Makes it a secondary window in the layers
+level1_window.title("PHANTASM")
+level1_window.geometry("600x800")
+level1_window.withdraw()  # just hides the window but it still exists
+level1_window.mainloop()
+#write your widgets for the level1 here and stuff
 
 
-
+#Level 2
